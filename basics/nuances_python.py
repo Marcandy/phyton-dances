1c0f8f72bfb0f78d1a6a8bfc73b23a1b7c876b16
marcandy <mnoeljeu@jcp.com>
2019-02-28 15:07:53 -0600

    basic continuation

--- basics/nuances_python.py
+++ basics/nuances_python.py
@@ -18,4 +18,55 @@ print(sampleStr[-2])
 # \" for adding a double quote
 # \n for a new line
 
+print(sampleStr.upper())
 
+# check is a string if part of a string exist in another -- we use the in keyword
+
+"man" in sampleStr
+"sade" not in sampleStr
+
+# bin
+
+# complex numbers in python
+
+x = 1 + 2j
+
+# python // double division give integer result
+
+print(7 // 3)
+
+
+# in python there are no constants -- we use upper case letters to let others
+# we use upper case letters to let other developer know that a variable should be constant
+
+PI = 943280
+import math
+# allows complex mathematical operation
+
+math.degrees(43)
+
+# one more thing is python is a strongly typed language
+# it does not do any type conversion
+
+z = input("x: ")
+
+print(int(z))
+print(float(z))
+print(bool(z))
+
+# falsey values in python
+# Falsy
+# ""
+# []
+# None (null)
+
+if y >= 4:
+    pass # use pass in python to have an empty block, basically to pass
+else:
+    pass
+
+
+#we use indentation in python for block of code
+emptyPan = ""
+if not emptyPan:
+    print("Emptypan is a sring")
