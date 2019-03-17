0620dd839608b1cf8711bead60a0454765d585c9
marcandy <mnoeljeu@jcp.com>
2019-03-17 18:17:17 -0500

    created views for all articles and single article

--- djreact/backend/src/articles/api/serializers.py
+++ djreact/backend/src/articles/api/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 
-from Article.models import Article
+from articles.models import Article
 
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        field = ('title', 'content')
+        fields = ('title', 'content')
