86def59fbfe44396390f50db0b48460756686c54
marcandy <mnoeljeu@jcp.com>
2019-03-17 01:59:20 -0500

    [djreat] initial set up of code structure

---
+++ djreact/backend/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mnoeljeu/Documents/python-dances/djreact/backend/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
