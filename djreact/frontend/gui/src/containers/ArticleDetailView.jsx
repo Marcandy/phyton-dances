af9dad39043c78b66d29f8ee490da1c6087e7482
marcandy <mnoeljeu@jcp.com>
2019-03-17 20:20:53 -0500

    created detailview component with react router to see a single article car

---
+++ djreact/frontend/gui/src/containers/ArticleDetailView.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import { Card } from "antd";
+
+class ArticleDetailView extends Component {
+  state = {
+    article: {}
+  };
+
+  componentDidMount() {
+    const articleID = this.props.match.params.articleID;
+    axios.get(`http://127.0.0.1:8000/api/${articleID}`).then(res => {
+      this.setState({
+        article: res.data
+      });
+    });
+  }
+  render() {
+    return (
+      <Card title={this.state.article.title}>{this.state.article.content}</Card>
+    );
+  }
+}
+
+export default ArticleDetailView;
